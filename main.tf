d0374956fc4dae9d51eb5c3c4849965a16b41228
Carlos Fuentes <167764847+cfuentesc01@users.noreply.github.com>
2025-03-20 15:42:21 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -103,7 +103,7 @@ resource "aws_route_table" "private" {
   }
 }
 
-// Creando instancias
+// Creación de la instancia de Nginx - 1
 
 resource "aws_instance" "nginx_1" {
   ami           = "ami-12345678"
@@ -115,6 +115,8 @@ resource "aws_instance" "nginx_1" {
   }
 }
 
+// Creación de la instancia de Nginx - 2
+
 resource "aws_instance" "nginx_2" {
   ami           = "ami-12345678"
   instance_type = "t2.micro"
@@ -125,15 +127,19 @@ resource "aws_instance" "nginx_2" {
   }
 }
 
+// Creación de la instancia de Lemmy
+
 resource "aws_instance" "lemmy" {
   ami           = "ami-12345678"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.private_3.id
 
   tags = {
-    Name = "LEMMY"
+    Name = "LEMMY-1"
   }
 }
+
+// Creación de la instancia de Gancio
 
 resource "aws_instance" "gancio" {
   ami           = "ami-12345678"
@@ -141,7 +147,7 @@ resource "aws_instance" "gancio" {
   subnet_id     = aws_subnet.private_4.id
 
   tags = {
-    Name = "GANCIO"
+    Name = "GANCIO-1"
   }
 }
 
@@ -212,5 +218,135 @@ resource "aws_security_group" "rds_sg_gancio" {
     to_port     = 3306
     protocol    = "tcp"
     cidr_blocks = ["10.208.0.0/16"]  # Permitir conexiones dentro de la VPC
+  }
+}
+
+
+
+
+
+
+
+
+// Instancia para hacer backups
+resource "aws_instance" "backup_server" {
+  ami                    = "ami-12345678" 
+  instance_type          = "t2.micro"  
+  subnet_id              = aws_subnet.private_3.id
+  vpc_security_group_ids = [aws_security_group.backup_sg.id]
+  key_name               = "mi-clave-aws" # Sustituye por tu par de claves
+  root_block_device {
+    volume_size = 20
+  }
+
+  tags = {
+    Name = "Servidor de Backups"
+  }
+}
+
+// Grupo de seguridad para instancias de Backups
+resource "aws_security_group" "backup_sg" {
+  vpc_id = aws_vpc.tfg_asir_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.208.0.0/16"]  # Solo accesible dentro de la VPC
+  }
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.208.0.0/16"]  # Acceso solo a MySQL dentro de la VPC
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+// Volúmenes EBS para RAID 1 (50GB x 2)
+resource "aws_ebs_volume" "backup_raid_1" {
+  availability_zone = aws_instance.backup_server.availability_zone
+  size             = 50
+  type             = "gp3"
+
+  tags = {
+    Name = "RAID1-1"
+  }
+}
+
+resource "aws_ebs_volume" "backup_raid_2" {
+  availability_zone = aws_instance.backup_server.availability_zone
+  size             = 50
+  type             = "gp3"
+
+  tags = {
+    Name = "RAID1-2"
+  }
+}
+
+// Volumen adicional de 50GB para almacenamiento
+resource "aws_ebs_volume" "backup_secondary" {
+  availability_zone = aws_instance.backup_server.availability_zone
+  size             = 50
+  type             = "gp3"
+
+  tags = {
+    Name = "Backup-Extra"
+  }
+}
+
+// Adjuntar volúmenes RAID 1 a la instancia
+resource "aws_volume_attachment" "attach_raid_1" {
+  device_name = "/dev/xvdf"
+  instance_id = aws_instance.backup_server.id
+  volume_id   = aws_ebs_volume.backup_raid_1.id
+}
+
+resource "aws_volume_attachment" "attach_raid_2" {
+  device_name = "/dev/xvdg"
+  instance_id = aws_instance.backup_server.id
+  volume_id   = aws_ebs_volume.backup_raid_2.id
+}
+
+// Adjuntar volumen secundario de 50GB
+resource "aws_volume_attachment" "attach_secondary" {
+  device_name = "/dev/xvdh"
+  instance_id = aws_instance.backup_server.id
+  volume_id   = aws_ebs_volume.backup_secondary.id
+}
+
+// Creación del bucket S3
+resource "aws_s3_bucket" "backup_bucket" {
+  bucket = "tfg-backups-mysql"
+
+  tags = {
+    Name        = "MySQL Backups"
+    Environment = "Production"
+  }
+}
+
+// Configurando Glacier
+resource "aws_s3_bucket_lifecycle_configuration" "backup_lifecycle" {
+  bucket = aws_s3_bucket.backup_bucket.id
+
+  rule {
+    id     = "MoveToGlacier"
+    status = "Enabled"
+
+    transition {
+      days          = 30
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = 365  # Borrar backups después de un año
+    }
   }
 }
