3663570e32c6421832cb5d7ef991ace06c640445
Carlos Fuentes <167764847+cfuentesc01@users.noreply.github.com>
2025-03-29 12:44:05 +0100

    Update nginx-2.sh

--- scripts/nginx-2.sh
+++ scripts/nginx-2.sh
@@ -36,12 +36,21 @@ server {
     try_files $uri @proxy;
   }
 
-  location @proxy {
+location @proxy {
+    proxy_pass http://10.208.4.70:13120;
+    proxy_redirect / /;
     proxy_set_header Host $host;
+    proxy_set_header X-Real-IP $remote_addr;
     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
     proxy_set_header X-Forwarded-Proto $scheme;
-    proxy_pass http://127.0.0.1:13120;
-  }
+
+    # Soporte para WebSockets
+    proxy_set_header Upgrade $http_upgrade;
+    proxy_set_header Connection "Upgrade";
+}
+
+  error_log /var/log/nginx/gancio_error.log;
+  access_log /var/log/nginx/gancio_access.log;
 }
 EOF
 
