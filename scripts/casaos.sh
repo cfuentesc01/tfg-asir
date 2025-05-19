2311cefe4f1a07965aecc87d88ce48b17221bcdf
carloosfuentes <cafu2005@gmail.com>
2025-05-19 23:24:27 +0200

    Update casaos.sh

--- scripts/casaos.sh
+++ scripts/casaos.sh
@@ -1,5 +1,46 @@
 #!/bin/bash
 
+# Actualizar sistema
+sudo apt update && sudo apt upgrade -y
+
+# Instalar CasaOS
+curl -fsSL https://get.casaos.io | sudo bash
+
+# Monitorización
+sudo useradd --no-create-home --shell /bin/false prometheus
+sudo chown -R prometheus:prometheus /opt/node_exporter
+
+# Instalación de Node Exporter
+sudo wget https://github.com/prometheus/node_exporter/releases/download/v1.3.0/node_exporter-1.3.0.linux-amd64.tar.gz -P /opt
+sudo tar -xvf /opt/node_exporter-1.3.0.linux-amd64.tar.gz
+sudo mv node_exporter-1.3.0.linux-amd64/ /opt/node_exporter
+
+# Creación del servicio
+sudo tee /etc/systemd/system/node_exporter.service > /dev/null << EOF
+[Unit]
+Description=node_exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=prometheus
+Group=prometheus
+Type=simple
+ExecStart=/opt/node_exporter/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl daemon-reexec
+sudo systemctl daemon-reload
+sudo systemctl enable node_exporter
+sudo systemctl start node_exporter
+
+sudo tee /home/admin/raid.sh > /dev/null << EOF
+#!/bin/bash
+
 # Creación del raid
 set -e
 
@@ -40,41 +81,4 @@ echo "UUID=$UUID $MOUNT_POINT ext4 defaults,nofail,discard 0 0" | sudo tee -a /e
 echo "[+] RAID 1 montado correctamente en $MOUNT_POINT"
 
 #cat /proc/mdstat
-
-# Actualizar sistema
-sudo apt update && sudo apt upgrade -y
-
-# Instalar CasaOS
-curl -fsSL https://get.casaos.io | sudo bash
-
-# Monitorización
-sudo useradd --no-create-home --shell /bin/false prometheus
-sudo chown -R prometheus:prometheus /opt/node_exporter
-
-# Instalación de Node Exporter
-sudo wget https://github.com/prometheus/node_exporter/releases/download/v1.3.0/node_exporter-1.3.0.linux-amd64.tar.gz -P /opt
-sudo tar -xvf /opt/node_exporter-1.3.0.linux-amd64.tar.gz
-sudo mv node_exporter-1.3.0.linux-amd64/ /opt/node_exporter
-
-# Creación del servicio
-sudo tee /etc/systemd/system/node_exporter.service > /dev/null << EOF
-[Unit]
-Description=node_exporter
-Wants=network-online.target
-After=network-online.target
-
-[Service]
-User=prometheus
-Group=prometheus
-Type=simple
-ExecStart=/opt/node_exporter/node_exporter
-
-[Install]
-WantedBy=multi-user.target
 EOF
-
-sudo systemctl daemon-reload
-sudo systemctl daemon-reexec
-sudo systemctl daemon-reload
-sudo systemctl enable node_exporter
-sudo systemctl start node_exporter
